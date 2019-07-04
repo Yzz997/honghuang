8ed5d0b8641ad7ad4cc58680071bfd44df27a699
Yzz997 <zhiy6@outlook.com>
2019-07-04 08:51:33 +0800

    this is a shell file

---
+++ autoinstall.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# this is a auto install shell by zzy
+# 设置路径
+PATH = /home/yzz/Downloads/try/
+export PATH
+
+echo '下面即将开始暴风雨式的安装！！！'
+sleep 2
+
+wget -c http://soft.vpser.net/lnmp/lnmp1.6.tar.gz
+tar -xzvf lnmpq1.6.tar.gz
+cd ./lnmpq1.6
+vim README
+
